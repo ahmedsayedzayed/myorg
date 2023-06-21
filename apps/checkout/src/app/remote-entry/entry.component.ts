52c1cf6f38b0d297e4cdb599129ae357c78f8999
ahmedsayedzayed <hmada_7@icloud.com>
2023-06-21 09:38:23 +0200

    init commit

---
+++ apps/checkout/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myorg-checkout-entry',
+  template: `<myorg-nx-welcome></myorg-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/product/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myorg-product-entry',
+  template: `<myorg-nx-welcome></myorg-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
